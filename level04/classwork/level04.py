04cfd440d4a6cd298cee55e0d5d6986490543f8a
lukausupashvili <lukausupashvili@gmail.com>
2024-07-10 14:57:43 +0400

    second commit

---
+++ level04/classwork/level04.py
@@ -0,0 +1,7 @@
+name = input("enter your name: ")
+
+print("hello" + name)
+
+#int str float date  type
+#მთელი რიცხვი
+#
---
+++ level04/homework/level04.py
@@ -0,0 +1,14 @@
+#user_name = input("hello luka: ")
+
+
+#print(f"Hello, {user_name}!")
+
+first_name = input("luka: ")
+
+last_name = input("usupashvili: ")
+
+age = input("16: ")
+
+city = input("tbilisi: ")
+
+print(f"Hello, {first_name} {last_name}. You are {age} years old and live in {city}.")
